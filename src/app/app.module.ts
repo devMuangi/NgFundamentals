704639f698bc2ed16bc86359f53a600cb1a5e14f
devMuangi <devmuangi@gmail.com>
2020-01-11 16:39:26 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { EventsAppComponent } from './events-app.component';
+
+import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+//import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
+@NgModule({
+  imports: [
+    BrowserModule
+  ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    NavBarComponent
+  ],
+  providers: [EventService],
+  bootstrap: [EventsAppComponent]
+})
+export class AppModule { }
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { EventService } from './shared/event.service';
+
+@Component({
+  selector: 'events-list',
+  template:
+  `
+<div>
+  <h1>Upcoming Angular Events</h1>
+  <hr>
+  <div class="row">
+  <div *ngFor="let event of events" class="col-md-5">
+    <event-thumbnail [event]="event"></event-thumbnail>
+  </div>
+
+  </div>
+
+</div>
+
+  `
+})
+export class EventsListComponent {
+  events: any[];
+  constructor(private eventService: EventService) {
+    this.events = this.eventService.getEvents();
+  }
+}
